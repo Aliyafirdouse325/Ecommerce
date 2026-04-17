cafa20640573b1dcfab245e7ce8181e9bc451306
Aliyafirdouse325 <aliyafirdouseaf3@gmail.com>
2026-04-17 16:27:58 +0530

    all files

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://ecommerce-5zii.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
